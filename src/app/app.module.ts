582f681ee82e7840aa1f0d57c607bb6cb58ac43f
Platiplus <quazarweb@gmail.com>
2018-03-14 15:11:12 -0300

    FireStore Integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAkVJPc2ITILqxIcsiw-sm3gyNaHJhP0sg",
+    authDomain: "geladeira-bed71.firebaseapp.com",
+    databaseURL: "https://geladeira-bed71.firebaseio.com",
+    projectId: "geladeira-bed71",
+    storageBucket: "",
+    messagingSenderId: "842101763955"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
